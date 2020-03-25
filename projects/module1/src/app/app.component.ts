9d10d408df9778ff341535ae10cd869f6e1c4bec
Alexander Lebedev <exiragor@yandex.ru>
2020-03-25 16:18:43 +0300

    homework

--- projects/module1/src/app/app.component.ts
+++ projects/module1/src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { IProduct, product } from '../mocks/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private _productInCart: IProduct|null = null;
+
+  public getProduct(): IProduct {
+    return product;
+  }
+
+  public getProductInCart(): IProduct|undefined {
+    return this._productInCart || undefined;
+  }
+
+  public setProductInCart(p: IProduct) {
+    this._productInCart = p;
+  }
+}
--- projects/module1/src/app/card/card.component.ts
+++ projects/module1/src/app/card/card.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../../mocks/products';
 
 @Component({
   selector: 'app-shop-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {}
+export class CardComponent {
+
+  @Input()
+  public product: IProduct|null = null;
+
+  @Output()
+  public addToCart = new EventEmitter();
+
+  public addProduct(): void {
+    this.addToCart.emit(this.product);
+  }
+}
